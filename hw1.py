5e923b36c403f1c6337e40bdba56eee04ca98e1e
Kabilan Senapathy <kabilansenapathy@live.in>
2019-09-22 10:35:52 -0400

    Code clean up

--- hw1.py
+++ hw1.py
@@ -1,14 +1,10 @@
-import sys
-# import pandas as pd
-
 allprices = {}
 tks = ['IBM', 'MSFT', 'GOOG', 'AAPL', 'AMZN', 'FB']
-
 
 def getDataIntoDict(file, dict):
 
     _data = open(file)
-    data = []
+    data = data_array = []
 
     with _data as f:
         data = _data.readlines()
@@ -17,53 +13,37 @@ def getDataIntoDict(file, dict):
         data[i] = data[i].strip("\n")
 
     data.pop(0)
-    data_array = []
+    
     for i in range(len(data)):
         data_array.append(data[i].split(','))
 
     stock_price = []
 
     for i in range(len(data_array)):
-        stock_price.append(data_array[i][5])
+        stock_price.append(float(data_array[i][5]))
 
     allprices[file.split('.')[0]] = stock_price
 
 
 def corellation(X, Y):
-    mean_x = 0
-    mean_y = 0
+    mean_x = mean_y = sum_numer = s_x = s_y = 0
+
+    mean_x = sum(X,0)/len(X)
+    mean_y = sum(Y,0)/len(Y)
 
     for i in range(len(X)):
-        mean_x = mean_x+float(X[i])
-        mean_y = mean_y+float(Y[i])
-
-    mean_x = (mean_x/len(X))
-    mean_y = (mean_y/len(Y))
-    # print("mean_x",mean_x)
-    # print("mean_y",mean_y)
-    sum_numer = 0
-    s_x = 0
-    s_y = 0
-
-    for i in range(len(X)):
-        sum_numer += ((float(X[i])-mean_x)*(float(Y[i])-mean_y))
-
-    for i in range(len(X)):
-        s_x += (float(X[i])-mean_x)**2
-        s_y += (float(Y[i])-mean_y)**2
-
-    return (sum_numer/(s_x*s_y)**0.5)
-
+        sum_numer += ((X[i])-mean_x)*((Y[i])-mean_y)
+        s_x += ((X[i])-mean_x)**2
+        s_y += ((Y[i])-mean_y)**2
+        
+    return round((sum_numer/(s_x*s_y)**0.5),2)
 
 for i in range(len(tks)):
     getDataIntoDict(tks[i]+'.csv', allprices)
 
-
 def returnCombs(item, list):
     for i in range(len(list)):
         if(item != list[i]):
-            # print(item, ":" ,list[i])
-            print(item+":"+list[i],corellation(allprices[item], allprices[list[i]]))
+            print(item+":"+list[i]+" = ",corellation(allprices[item], allprices[list[i]]))
 for i in range(len(tks)):
     returnCombs(tks[i], tks[i:])
-
